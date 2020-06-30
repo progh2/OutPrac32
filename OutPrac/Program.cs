ff6fdb2e6ef10c0e98dc181c73be96d2a2f377c7
Gihun Ham <ham@e-mirim.hs.kr>
2020-06-30 14:25:51 +0900

    초기 커밋.

---
+++ OutPrac/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutPrac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("사용자 입력: ");
+            int output;
+            bool result = int.TryParse(
+                Console.ReadLine(),
+                out output);
+            if (result)
+            {
+                Console.WriteLine("값: " + output);
+            }
+            else
+            {
+                Console.WriteLine("정수를 입력하세요!");
+            }
+        }
+    }
+}
